f6136625351ca5c7c8a9a40aa35df5f5c68ab070
edmoiseenkoff <zdedik95@gmail.com>
2015-12-08 13:00:26 +0200

    ! fix: popover position

--- src/js/views/PopoverView.js
+++ src/js/views/PopoverView.js
@@ -6,7 +6,7 @@
 	Module.PopoverView = Module.Class(parent, {
 
 		popoverEl: null,
-		arrowEl: null,
+		triangleEl: null,
 		toggleEl: null,
 
 		active: false,
@@ -24,11 +24,11 @@
 
 			this.el = document.createElement('div');
 
-			this.arrowEl = document.createElement('div');
-			this.arrowEl.className = 'sp-arrow';
+			this.triangleEl = document.createElement('div');
+			this.triangleEl.className = 'sp-arrow';
 
 			this.popoverEl.appendChild(this.el);
-			this.popoverEl.appendChild(this.arrowEl);
+			this.popoverEl.appendChild(this.triangleEl);
 
 			this.handleClickSticker((function() {
 				this.toggle();
@@ -83,22 +83,31 @@
 		positioned: function() {
 			var arrowOffset = 0;
 
-			if (this.arrowEl) {
+			// todo: check
+			if (this.triangleEl) {
 				var style = this.toggleEl.currentStyle || window.getComputedStyle(this.toggleEl);
 				var marginLeft = parseInt(style.marginLeft, 10);
 
-				this.arrowEl.style.marginLeft = (this.toggleEl.clientWidth / 2) - (this.arrowEl.offsetWidth / 2) + marginLeft + 'px';
+				this.popoverEl.style.marginLeft = marginLeft + 'px';
 
-				var arrowStyle = this.arrowEl.currentStyle || window.getComputedStyle(this.arrowEl);
+				if (this.popoverEl.getBoundingClientRect().left + this.popoverEl.offsetWidth > window.innerWidth) {
+					this.popoverEl.style.marginLeft = marginLeft - (this.popoverEl.offsetWidth / 2) + (this.toggleEl.clientWidth / 2) + 'px';
+				}
+
+				this.triangleEl.style.marginLeft = this.toggleEl.getBoundingClientRect().left
+					- this.popoverEl.getBoundingClientRect().left
+					+ (this.toggleEl.clientWidth / 2) - (this.triangleEl.offsetWidth / 2)
+					+ 'px';
+
+				var arrowStyle = this.triangleEl.currentStyle || window.getComputedStyle(this.triangleEl);
 				if (arrowStyle.display != 'none') {
 					arrowOffset = 15;
 				}
-			} else {
-				console && console.error('error');
-			}
 
-			if (this.arrowEl)
-			this.popoverEl.style.top = -(this.popoverEl.offsetHeight + arrowOffset) + 'px';
+				this.popoverEl.style.marginTop = -(this.popoverEl.offsetHeight + this.toggleEl.offsetHeight + arrowOffset) + 'px';
+			} else {
+				console && console.error('error: triangle not found');
+			}
 		}
 
 	});
