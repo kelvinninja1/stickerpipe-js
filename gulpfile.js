58f3b3f7d7e3660a4c333b4c2645bc71747a57ee
edmoiseenkoff <zdedik95@gmail.com>
2015-12-10 11:19:34 +0200

    ~ move: to demo folder

--- gulpfile.js
+++ gulpfile.js
@@ -10,19 +10,19 @@ var gulp = require('gulp'),
 gulp.task('default', ['watcher'], function () {});
 
 gulp.task('watcher', function () {
-	gulp.watch(['work/assets/scss/**/*.scss'], ['css']);
+	gulp.watch(['work/demo/assets/scss/**/*.scss'], ['css']);
 });
 
 gulp.task('css', ['clean:css'], function () {
-	return gulp.src('work/assets/scss/**/*.scss')
+	return gulp.src('work/demo/assets/scss/**/*.scss')
 		.pipe(sass())
 		.pipe(autoprefixer())
-		.pipe(gulp.dest('work/assets/css'))
+		.pipe(gulp.dest('work/demo/assets/css'))
 		//.pipe(git.add())
 		;
 });
 
 gulp.task('clean:css', function() {
-	return gulp.src('work/assets/css', {read: false})
+	return gulp.src('work/demo/assets/css', {read: false})
 		.pipe(clean());
 });
