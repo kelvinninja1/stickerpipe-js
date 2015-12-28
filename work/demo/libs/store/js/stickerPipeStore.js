ffe09146364a1ca219cf52e88e1592b82927dbb0
edmoiseenkoff <zdedik95@gmail.com>
2015-12-28 15:06:31 +0200

    ~

--- work/demo/libs/store/js/stickerPipeStore.js
+++ work/demo/libs/store/js/stickerPipeStore.js
@@ -1,38 +1,3 @@
-
-var appStickerPipeStore = angular.module('appStickerPipeStore', [
-	'ngRoute',
-	'angular-google-analytics',
-	'partials',
-	'environment'
-]);
-
-appStickerPipeStore.run(function($rootScope, PlatformAPI) {
-
-	PlatformAPI.init();
-
-	$rootScope.$on('$routeChangeStart', function() {
-		PlatformAPI.showInProgress(true);
-	});
-
-	$rootScope.$on('$routeChangeSuccess', function() {
-		PlatformAPI.showInProgress(false);
-		$rootScope.error = false;
-	});
-
-	$rootScope.$on('$routeChangeError', function(e, c, p, error) {
-		PlatformAPI.showInProgress(false);
-		$rootScope.error = true;
-	});
-});
-
-appStickerPipeStore.controller('AppController', function(Config, envService) {
-
-	document.body.addEventListener('touchstart',function(){},false);
-
-	if (envService.is('local') || envService.is('development')) {
-		document.getElementById('css').setAttribute('href', envService.read('cssUrl') + Config.platform.toLocaleLowerCase() + '.css?v='+(+(new Date())));
-	}
-});
 angular.module('environment',[]).provider('envService',function(){this.environment='development';this.data={};this.config=function(config){this.data=config;};this.set=function(environment){this.environment=environment;};this.get=function(){return this.environment;};this.read=function(variable){if(variable!=='all'){return this.data.vars[this.get()][variable];}
 	return this.data.vars[this.get()];};this.is=function(environment){return(environment===this.environment);};this.check=function(){var	location=window.location.href,self=this;angular.forEach(this.data.domains,function(v,k){angular.forEach(v,function(v){if(location.match('//'+v)){self.environment=k;}});});};this.$get=function(){return this;};});
 /**
@@ -58,6 +23,43 @@ angular.module('environment',[]).provider('envService',function(){this.environme
     e,u,b)})}function k(){var a,b;c.forEach(g,function(d,g){var q;if(q=!b){var h=f.path();q=d.keys;var l={};if(d.regexp)if(h=d.regexp.exec(h)){for(var k=1,m=h.length;k<m;++k){var n=q[k-1],p=h[k];n&&p&&(l[n.name]=p)}q=l}else q=null;else q=null;q=a=q}q&&(b=r(d,{params:c.extend({},f.search(),a),pathParams:a}),b.$$route=d)});return b||g[null]&&r(g[null],{params:{},pathParams:{}})}function t(a,b){var d=[];c.forEach((a||"").split(":"),function(a,c){if(0===c)d.push(a);else{var f=a.match(/(\w+)(?:[?*])?(.*)/),
     g=f[1];d.push(b[g]);d.push(f[2]||"");delete b[g]}});return d.join("")}var w=!1,n,v,s={routes:g,reload:function(){w=!0;a.$evalAsync(function(){l();m()})},updateParams:function(a){if(this.current&&this.current.$$route)a=c.extend({},this.current.params,a),f.path(t(this.current.$$route.originalPath,a)),f.search(a);else throw B("norout");}};a.$on("$locationChangeStart",l);a.$on("$locationChangeSuccess",m);return s}]});var B=c.$$minErr("ngRoute");p.provider("$routeParams",function(){this.$get=function(){return{}}});
     p.directive("ngView",v);p.directive("ngView",A);v.$inject=["$route","$anchorScroll","$animate"];A.$inject=["$compile","$controller","$route"]})(window,window.angular);
+
+var appStickerPipeStore = angular.module('appStickerPipeStore', [
+	'ngRoute',
+	'angular-google-analytics',
+	'partials',
+	'environment'
+]);
+
+appStickerPipeStore.run(function($rootScope, PlatformAPI, $window) {
+
+	PlatformAPI.init();
+
+	$rootScope.$on('$routeChangeStart', function() {
+		PlatformAPI.showInProgress(true);
+	});
+
+	$rootScope.$on('$routeChangeSuccess', function() {
+		PlatformAPI.showInProgress(false);
+		$rootScope.error = false;
+		$window.scrollTo(0, 0);
+	});
+
+	$rootScope.$on('$routeChangeError', function(e, c, p, error) {
+		PlatformAPI.showInProgress(false);
+		$rootScope.error = true;
+		$window.scrollTo(0, 0);
+	});
+});
+
+appStickerPipeStore.controller('AppController', function(Config, envService) {
+
+	document.body.addEventListener('touchstart',function(){},false);
+
+	if (envService.is('local') || envService.is('development')) {
+		document.getElementById('css').setAttribute('href', envService.read('cssUrl') + Config.platform.toLocaleLowerCase() + '.css?v='+(+(new Date())));
+	}
+});
 appStickerPipeStore.config(function(envServiceProvider) {
 
 	// default development(work)
@@ -503,63 +505,6 @@ appStickerPipeStore.factory('PlatformAPI', function(Config, $injector, $route, $
 
 });
 
-appStickerPipeStore.directive('basePage', function() {
-
-	return {
-		restrict: 'AE',
-		templateUrl: '/modules/base-page/view.tpl',
-		link: function($scope, $el, attrs) {}
-	};
-});
-
-appStickerPipeStore.controller('PackController', function($scope, Config, EnvConfig, PlatformAPI, i18n, $rootScope, PackService, pack) {
-
-	angular.extend($scope, {
-		config: Config,
-		platformAPI: PlatformAPI,
-		pack: pack,
-		i18n: i18n,
-		packService: PackService,
-		showActionProgress: false,
-
-		getStickerUrl: function(name) {
-			return EnvConfig.stickersStorageUrl + this.pack.pack_name + '/' + name + '_' + Config.resolutionType + '.png';
-		},
-
-		getMainStickerUrl: function() {
-			return $scope.getStickerUrl('main_icon');
-		},
-
-		showCollections: function() {
-			PlatformAPI.showCollections(pack.pack_name);
-		},
-
-		purchasePack: function() {
-			$scope.showActionProgress = true;
-			PlatformAPI.purchasePack(pack.title, pack.pack_name, pack.pricepoint);
-		}
-	});
-
-	$rootScope.$on('hideActionProgress', function() {
-		$scope.showActionProgress = false;
-		if(!$scope.$$phase) {
-			$scope.$apply();
-		}
-	});
-});
-
-appStickerPipeStore.controller('StoreController', function($scope, packs, Config, PlatformAPI) {
-
-	angular.extend($scope, {
-		platformAPI: PlatformAPI,
-		packs: packs.packs,
-
-		getPackMainIcon: function(pack) {
-			return pack.main_icon[Config.resolutionType];
-		}
-	});
-});
-
 appStickerPipeStore.value('En', {
 	download: 'Download',
 	open: 'Open',
@@ -574,6 +519,15 @@ appStickerPipeStore.value('Ru', {
 	buyPack: 'Купить',
 	unavailableContent: 'В данный момент этот контент недоступен',
 	get: 'Скачать'
+});
+
+appStickerPipeStore.directive('basePage', function() {
+
+	return {
+		restrict: 'AE',
+		templateUrl: '/modules/base-page/view.tpl',
+		link: function($scope, $el, attrs) {}
+	};
 });
 
 appStickerPipeStore.factory('AndroidPlatform', function() {
@@ -691,6 +645,54 @@ appStickerPipeStore.factory('JSPlatform', function($rootScope, $window, $timeout
 			this.showCollections(attrs.packName)
 		}
 
+	});
+});
+
+appStickerPipeStore.controller('PackController', function($scope, Config, EnvConfig, PlatformAPI, i18n, $rootScope, PackService, pack) {
+
+	angular.extend($scope, {
+		config: Config,
+		platformAPI: PlatformAPI,
+		pack: pack,
+		i18n: i18n,
+		packService: PackService,
+		showActionProgress: false,
+
+		getStickerUrl: function(name) {
+			return EnvConfig.stickersStorageUrl + this.pack.pack_name + '/' + name + '_' + Config.resolutionType + '.png';
+		},
+
+		getMainStickerUrl: function() {
+			return $scope.getStickerUrl('main_icon');
+		},
+
+		showCollections: function() {
+			PlatformAPI.showCollections(pack.pack_name);
+		},
+
+		purchasePack: function() {
+			$scope.showActionProgress = true;
+			PlatformAPI.purchasePack(pack.title, pack.pack_name, pack.pricepoint);
+		}
+	});
+
+	$rootScope.$on('hideActionProgress', function() {
+		$scope.showActionProgress = false;
+		if(!$scope.$$phase) {
+			$scope.$apply();
+		}
+	});
+});
+
+appStickerPipeStore.controller('StoreController', function($scope, packs, Config, PlatformAPI) {
+
+	angular.extend($scope, {
+		platformAPI: PlatformAPI,
+		packs: packs.packs,
+
+		getPackMainIcon: function(pack) {
+			return pack.main_icon[Config.resolutionType];
+		}
 	});
 });
 
