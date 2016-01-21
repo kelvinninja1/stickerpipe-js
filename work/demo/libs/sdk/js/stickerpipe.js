335062424b56ed8f67576b90d336b92887c2654b
edmoiseenkoff <zdedik95@gmail.com>
2016-01-21 17:18:32 +0200

    ~ sdk

--- work/demo/libs/sdk/js/stickerpipe.js
+++ work/demo/libs/sdk/js/stickerpipe.js
@@ -3,7 +3,6 @@
 window.StickersModule = {};
 
 
-window.StickersModule.Libs = {};
 window.StickersModule.Utils = {};
 document.addEventListener("DOMContentLoaded", function(event) {
 
@@ -828,85 +827,6 @@ if ("document" in self) {
 	};
 
 })(window);
-
-(function(Module) {
-
-	var touch_x, touch_y, obj_x, obj_y, speed_x=0, speed_y=0, scrollanim;
-
-	function onTouchStart(e) {
-		clearInterval(scrollanim);
-		// Get Touch target
-		obj_x = e.target;
-		obj_y = e.target;
-		// Get the target parent that is scrollable
-		while ((window.getComputedStyle(obj_x)['overflow-x'] != "auto" && window.getComputedStyle(obj_x)['overflow-x'] != "scroll") || obj_x.parentNode == null) {
-			obj_x = obj_x.parentNode
-		}
-		while ((window.getComputedStyle(obj_y)['overflow-y'] != "auto" && window.getComputedStyle(obj_y)['overflow-y'] != "auto") || obj_y.parentNode == null) {
-			obj_y = obj_y.parentNode
-		}
-		// Get if no scrollable parents are present set null
-		if (obj_x.parentNode == null) obj_x = null;
-		if (obj_y.parentNode == null) obj_y = null;
-
-		// Get the touch starting point
-		var touch = e.touches[0];
-		touch_x = touch.pageX;
-		touch_y = touch.pageY;
-	}
-
-	function onTouchMove(e) {
-		// Clear animation
-		clearInterval(scrollanim);
-
-		// Prevent window scrolling
-		e.preventDefault();
-
-		// Scroll according to movement
-		var touch = e.touches[0];
-		obj_x.scrollLeft = obj_x.scrollLeft - (touch.pageX - touch_x);
-		obj_y.scrollTop = obj_y.scrollTop - (touch.pageY - touch_y);
-
-		// Set speed speed
-		speed_x = (touch.pageX - touch_x);
-		speed_y = (touch.pageY - touch_y);
-
-		// Set new positon
-		touch_x = touch.pageX;
-		touch_y = touch.pageY;
-	}
-
-	function onTouchEnd(e) {
-		// Clear previous animations
-		clearInterval(scrollanim);
-
-		// Animate
-		scrollanim = setInterval(function() {
-			obj_x.scrollLeft = obj_x.scrollLeft - speed_x;
-			obj_y.scrollTop = obj_y.scrollTop - speed_y;
-			// Decelerate
-			speed_x = speed_x * 0.9;
-			speed_y = speed_y * 0.9;
-
-			// Stop animation at the end
-			if (speed_x < 1 && speed_x > -1 && speed_y < 1 && speed_y > -1) clearInterval(scrollanim)
-		},15)
-	}
-
-	Module.Libs.Overscroll = {
-		start: function() {
-			document.addEventListener('touchstart', onTouchStart, false);
-
-			document.addEventListener('touchmove', onTouchMove, false);
-
-			document.addEventListener('touchend', onTouchEnd, false);
-		},
-
-		stop: function() {
-
-		}
-	};
-})(window.StickersModule);
 ;(function(window, undefined) {
 	"use strict";
 
