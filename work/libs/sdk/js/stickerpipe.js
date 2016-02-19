d4119d928392a260de0e025f98c4f7f15ddb4667
edmoiseenkoff <zdedik95@gmail.com>
2016-02-19 10:49:28 +0200

    ~

--- work/libs/sdk/js/stickerpipe.js
+++ work/libs/sdk/js/stickerpipe.js
@@ -2964,6 +2964,63 @@ window.StickersModule.Service = {};
 
 (function(Plugin) {
 
+	Plugin.Service.Ajax = function(options) {
+		options = options || {};
+
+		if (!options.url) {
+			return;
+		}
+
+		options.type = (options.type && options.type.toUpperCase()) || 'GET';
+		options.headers = options.headers || {};
+		options.data = options.data || {};
+		options.success = options.success || function() {};
+		options.error = options.error || function() {};
+		options.complete = options.complete || function() {};
+
+		options.headers.Apikey = Plugin.Configs.apiKey;
+		options.headers.Platform = 'JS';
+		options.headers.Localization = Plugin.Configs.lang;
+		options.headers.UserId = Plugin.Configs.userId;
+
+		if (options.type == 'POST' || options.type == 'PUT') {
+			options.headers['Content-Type'] = options.headers['Content-Type'] || 'application/json';
+			options.headers['DeviceId'] = Plugin.Service.Storage.getDeviceId();
+		}
+
+
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open(options.type, options.url, true);
+
+		for (var name in options.headers) {
+			xmlhttp.setRequestHeader(name, options.headers[name]);
+		}
+
+		xmlhttp.onreadystatechange = function() {
+			if (xmlhttp.readyState == 4) {
+				if (xmlhttp.status == 200) {
+					options.success(JSON.parse(xmlhttp.responseText), xmlhttp);
+				} else {
+					var response = {};
+					try {
+						response = JSON.parse(xmlhttp.responseText);
+					} catch (ex) {
+						response = {}
+					}
+					options.error(response, xmlhttp);
+				}
+
+				options.complete(JSON.parse(xmlhttp.responseText), xmlhttp);
+			}
+		};
+
+		xmlhttp.send(JSON.stringify(options.data));
+	};
+
+})(window.StickersModule);
+
+(function(Plugin) {
+
 	var API_VERSION = 2;
 
 	Plugin.Service.Api = {
@@ -2975,7 +3032,9 @@ window.StickersModule.Service = {};
 		getPacks: function(successCallback) {
 			var url = Plugin.Service.Url.getPacksUrl();
 
-			Plugin.Service.Http.get(url, {
+			Plugin.Service.Ajax({
+				type: 'get',
+				url: url,
 				success: function(response) {
 					response = response || {};
 					response.meta = response.meta || {};
@@ -2989,12 +3048,16 @@ window.StickersModule.Service = {};
 		},
 
 		sendStatistic: function(statistic) {
-			Plugin.Service.Http.post(Plugin.Service.Url.getStatisticUrl(), statistic);
+			Plugin.Service.Ajax({
+				type: 'post',
+				url: Plugin.Service.Url.getStatisticUrl(),
+				data: statistic
+			});
 		},
 
 		updateUserData: function(userData) {
-			return Plugin.Service.Http.ajax({
-				type: 'PUT',
+			return Plugin.Service.Ajax({
+				type: 'put',
 				url: Plugin.Service.Url.getUserDataUrl(),
 				data: userData,
 				headers: {
@@ -3004,7 +3067,9 @@ window.StickersModule.Service = {};
 		},
 
 		purchasePack: function(packName, pricePoint, successCallback) {
-			Plugin.Service.Http.post(Plugin.Service.Url.getPurchaseUrl(packName, pricePoint), {}, {
+			Plugin.Service.Ajax({
+				type: 'post',
+				url: Plugin.Service.Url.getPurchaseUrl(packName, pricePoint),
 				success: function(response) {
 					successCallback && successCallback(response.data);
 				},
@@ -3019,7 +3084,9 @@ window.StickersModule.Service = {};
 		},
 
 		getContentById: function(contentId, successCallback) {
-			Plugin.Service.Http.get(Plugin.Service.Url.getContentByIdUrl(contentId), {
+			Plugin.Service.Ajax({
+				type: 'get',
+				url: Plugin.Service.Url.getContentByIdUrl(contentId),
 				success: function(response) {
 					successCallback && successCallback(response.data);
 				}
@@ -3027,7 +3094,7 @@ window.StickersModule.Service = {};
 		},
 
 		hidePack: function(packName, successCallback) {
-			return Plugin.Service.Http.ajax({
+			return Plugin.Service.Ajax({
 				type: 'DELETE',
 				url: Plugin.Service.Url.getHidePackUrl(packName),
 				success: function(response) {
@@ -3311,97 +3378,6 @@ window.StickersModule.Service = {};
 			Plugin.Service.Event.changeContentHighlight(showContentHighlight);
 		}
 
-	};
-})(window.StickersModule);
-
-(function(Plugin) {
-
-	Plugin.Service.Http = {
-
-		// todo: refactor post(options) & get(options)
-
-		get: function(url, callbacks, headers) {
-			callbacks = callbacks || {};
-			headers = headers || {};
-
-			this.ajax({
-				type: 'GET',
-				url: url,
-				headers: headers,
-				success: callbacks.success,
-				error: callbacks.error,
-				complete: callbacks.complete
-			});
-		},
-
-		post: function(url, data, callbacks, headers) {
-			data = data || {};
-			callbacks = callbacks || {};
-			headers = headers || {};
-
-			this.ajax({
-				type: 'POST',
-				url: url,
-				data: data,
-				headers: headers,
-				success: callbacks.success,
-				error: callbacks.error,
-				complete: callbacks.complete
-			});
-		},
-
-		ajax: function(options) {
-			options = options || {};
-
-			if (!options.url) {
-				return;
-			}
-
-			options.type = (options.type && options.type.toUpperCase()) || 'GET';
-			options.headers = options.headers || {};
-			options.data = options.data || {};
-			options.success = options.success || function() {};
-			options.error = options.error || function() {};
-			options.complete = options.complete || function() {};
-
-			options.headers.Apikey = Plugin.Configs.apiKey;
-			options.headers.Platform = 'JS';
-			options.headers.Localization = Plugin.Configs.lang;
-			options.headers.UserId = Plugin.Configs.userId;
-
-			if (options.type == 'POST' || options.type == 'PUT') {
-				options.headers['Content-Type'] = options.headers['Content-Type'] || 'application/json';
-				options.headers['DeviceId'] = Plugin.Service.Storage.getDeviceId();
-			}
-
-
-			var xmlhttp = new XMLHttpRequest();
-			xmlhttp.open(options.type, options.url, true);
-
-			for (var name in options.headers) {
-				xmlhttp.setRequestHeader(name, options.headers[name]);
-			}
-
-			xmlhttp.onreadystatechange = function() {
-				if (xmlhttp.readyState == 4) {
-					if (xmlhttp.status == 200) {
-						options.success(JSON.parse(xmlhttp.responseText), xmlhttp);
-					} else {
-						var response = {};
-						try {
-							response = JSON.parse(xmlhttp.responseText);
-						} catch (ex) {
-							response = {}
-						}
-						options.error(response, xmlhttp);
-					}
-
-					options.complete(JSON.parse(xmlhttp.responseText), xmlhttp);
-				}
-			};
-
-			xmlhttp.send(JSON.stringify(options.data));
-		}
 	};
 })(window.StickersModule);
 
@@ -5009,7 +4985,7 @@ window.StickersModule.Module = {};
 			stickerpipe = _stickerpipe;
 		},
 
-		showCollections: function(data) {
+		showPack: function(data) {
 			Module.View.close();
 			stickerpipe.open(data.attrs.packName);
 		},
@@ -5102,6 +5078,7 @@ window.StickersModule.Module = {};
 
 		configureStore: function() {
 			callStoreMethod('configure', {
+				canShowPack: true,
 				canRemovePack: true
 			});
 		},
--- work/libs/store/index.php
+++ work/libs/store/index.php
@@ -19,7 +19,7 @@
 <!-- AngularJS -->
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular.min.js"></script>
 
-<script src="js/stickerPipeStore.js?v=1455818220815"></script>
+<script src="js/stickerPipeStore.js?v=1455871574189"></script>
 
 <script>
 	(function() {
