d044c1c702785998ce1f3ea2c14f53ef8e4bc90a
edmoiseenkoff <zdedik95@gmail.com>
2016-02-19 12:48:49 +0200

    ~

--- work/libs/store/index.php
+++ work/libs/store/index.php
@@ -19,7 +19,7 @@
 <!-- AngularJS -->
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular.min.js"></script>
 
-<script src="js/stickerPipeStore.js?v=1455876900288"></script>
+<script src="js/stickerPipeStore.js?v=1455878881344"></script>
 
 <script>
 	(function() {
