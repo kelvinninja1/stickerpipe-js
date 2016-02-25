f6b6ad35523654236e73cf4d963e055a3d1dcf29
edmoiseenkoff <zdedik95@gmail.com>
2016-02-25 18:17:40 +0200

    +

--- gulpfile.js
+++ gulpfile.js
@@ -3,109 +3,47 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	watch = require('gulp-watch'),
 	autoprefixer = require('gulp-autoprefixer'),
-	uglify = require('gulp-uglify'),
-	concat = require('gulp-concat'),
-	rename = require('gulp-rename'),
 	clean = require('gulp-clean'),
-	include = require('gulp-include'),
-	git = require('gulp-git'),
-	concatCss = require('gulp-concat-css'),
-	spritesmith = require('gulp.spritesmith'),
-	minifyCss = require('gulp-minify-css');
-
-var pluginName = 'stickerpipe',
-	exampleSrc = '../demo/work/libs/sdk';
+	spritesmith = require('gulp.spritesmith');
 
 // *** *** *** MAIN TASKS *** *** ****
 
 gulp.task('default', ['watcher'], function () {});
 
 gulp.task('watcher', function () {
-	gulp.watch(['src/scss/**/*.scss'], ['build:css']);
-	gulp.watch(['src/js/**/*.js'], ['build:js']);
-	gulp.watch(['src/img/icons/*.*'], ['build:img:icons']);
+	gulp.watch(['scss/**/*.scss'], ['css']);
 
-	gulp.watch(['build/**/*.*'], ['update:example']);
+	gulp.watch(['img/icons/*.*'], ['img:sprite']);
 });
 
-gulp.task('build:css', ['clean:css'], function () {
-	return gulp.src('src/scss/' + pluginName + '.scss')
+gulp.task('css', ['clean:css'], function () {
+	return gulp.src('scss/**/*.scss')
 		.pipe(sass())
 		.pipe(autoprefixer())
-		.pipe(concatCss(pluginName + '.css'))
-		.pipe(gulp.dest('build/css'))
-		.pipe(git.add())
-		.pipe(minifyCss())
-		.pipe(rename(function (path) {
-			path.basename += '.min';
-		}))
-		.pipe(gulp.dest('build/css'))
-		.pipe(git.add());
+		.pipe(gulp.dest('css'));
 });
 
-gulp.task('build:js', ['clean:js'], function() {
-	// todo: rename pluginName + '.js' --> app.js --> gulp.dest(... pluginName + '.js')
-	return gulp.src(['src/js/' + pluginName + '.js'])
-		.pipe(include())
-		.pipe(gulp.dest('build/js'))
-		.pipe(uglify({mangle: false}))
-		.pipe(rename(function (path) {
-			path.basename += '.min';
-		}))
-		.pipe(gulp.dest('build/js'));
+gulp.task('clean:css', function() {
+	return gulp.src('css', {read: false})
+		.pipe(clean());
 });
 
-gulp.task('build:img:icons', ['clean:img:icons'], function() {
-	var hash = (+new Date()),
-		img1x = 'icons.' + hash + '.png',
-		img2x = 'icons@2x.' + hash + '.png';
-
-
-	var spriteData = gulp.src('src/img/icons/*.*')
+gulp.task('img:sprite', function() {
+	var spriteData = gulp.src('img/icons/*.*')
 		.pipe(spritesmith({
 			cssName: '_icons.scss',
-			retinaSrcFilter: ['src/img/icons/*@2x.*'],
+			retinaSrcFilter: ['img/icons/*@2x.*'],
 
-			retinaImgName: img2x,
-			retinaImgPath: '../img/icons/' + img2x,
+			retinaImgName: 'icons_sprite@2x.png',
+			retinaImgPath: '../img/icons_sprite@2x.png',
 
-			imgName: img1x,
-			imgPath: '../img/icons/' + img1x,
+			imgName: 'icons_sprite.png',
+			imgPath: '../img/icons_sprite.png',
 			cssVarMap: function (sprite) {
-				sprite.name = 'sp-icon-' + sprite.name;
+				sprite.name = 'icon-' + sprite.name;
 			}
 		}));
 
-	spriteData.img
-		.pipe(gulp.dest('build/img/icons/')) // save sprite img file
-		.pipe(git.add());
-
-	return spriteData.css.pipe(gulp.dest('src/scss/')); // save scss file
-});
-
-gulp.task('update:example', ['clean:example'], function() {
-	return gulp.src(['build/**/*.*'])
-		.pipe(gulp.dest(exampleSrc));
-});
-
-
-// *** *** *** CLEANS *** *** ****
-
-gulp.task('clean:css', function() {
-	return gulp.src('build/css', {read: false})
-		.pipe(clean());
-});
-gulp.task('clean:js', function() {
-	return gulp.src('build/js', {read: false})
-		.pipe(clean());
-});
-gulp.task('clean:img:icons', function() {
-	return gulp.src('build/img/icons', {read: false})
-		.pipe(clean());
-});
-gulp.task('clean:example', function() {
-	return gulp.src(exampleSrc, {read: false})
-		.pipe(clean({
-			force: true
-		}));
+	spriteData.img.pipe(gulp.dest('img/')); // путь, куда сохраняем картинку
+	return spriteData.css.pipe(gulp.dest('scss/'));
 });
---
+++ libs/store/work/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" data-ng-app="appStickerPipeStore" data-ng-controller="AppController as appController">
+<head>
+	<meta charset="UTF-8">
+
+	<title>Storage</title>
+
+	<!-- Bootstrap -->
+<!--	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">-->
+<!--	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">-->
+
+	<link id="css" type="text/css" rel="stylesheet" href="css/<?=($_GET['style'] ?: 'js');?>" />
+</head>
+
+<body>
+
+<div data-base-page></div>
+
+<!-- AngularJS -->
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular.min.js"></script>
+
+<script src="js/stickerPipeStore.js?v=1456410502958"></script>
+
+<script>
+	(function() {
+		function valueOrNull(value) {
+			return (value && value != 'null') ? value : null;
+		}
+
+		appStickerPipeStore.constant('Config', {
+			apiKey: '<?=($_GET['apiKey'] ?: '1c0c4561cd005f6932f0de38934197ec');?>',
+			platform: '<?=($_GET['platform'] ?: 'Android');?>',
+			userId: '<?=($_GET['userId'] ?: '12345678901234567890123456789012');?>',
+			resolutionType: '<?=($_GET['density'] ?: 'xxhdpi');?>',
+			lang: '<?=($_GET['localization'] ?: 'ru');?>',
+			clientDomain: '<?=($_SERVER['HTTP_HOST'] ?: 'localhost');?>',
+			priceB: valueOrNull('<?=($_GET['priceB'] ?: 'null');?>'),
+			priceC: valueOrNull('<?=($_GET['priceC'] ?: 'null');?>'),
+			isSubscriber: ('<?=($_GET['is_subscriber'] ?: '0');?>' == '1'),
+			style: '<?=($_GET['style'] ?: 'js');?>',
+			primaryColor: '<?=($_GET['primaryColor'] ?: '');?>'
+		});
+	})();
+</script>
+
+</body>
+</html>
