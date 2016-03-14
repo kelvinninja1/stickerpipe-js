6c02410d0a7f4fec3609df8b2897b37f576598c8
edmoiseenkoff <zdedik95@gmail.com>
2016-03-14 11:34:06 +0200

    ~ update: web example

--- 908/web/js/stickerPipeWeb.js
+++ 908/web/js/stickerPipeWeb.js
@@ -38,6 +38,60 @@ module.run(['$templateCache', function($templateCache) {
 })();
 
 
+app.directive('audioSticker', function() {
+
+	return {
+		restrict: 'A',
+		scope: {
+			audioSticker: '='
+		},
+		templateUrl: '/directives/audioSticker/view',
+		link: function ($scope, $el, attrs) {
+
+			var audio = new Audio();
+			audio.preload = 'none';
+
+			$scope.showAudioProgress = false;
+			$scope.played = false;
+			$scope.loaded = false;
+
+			function play() {
+				$scope.played = true;
+				audio.play();
+
+				if (!$scope.$$phase) {
+					$scope.$apply();
+				}
+
+				ga('stickerTracker.send', 'event', 'Sound sticker', 'play', $scope.audioSticker.name);
+			}
+
+			audio.onloadeddata = function() {
+				$scope.showAudioProgress = false;
+				$scope.loaded = true;
+				play();
+			};
+
+			$el.find('.play')[0].onclick = function() {
+				if (!audio.src) {
+					audio.src = $scope.audioSticker.audio;
+					$scope.showAudioProgress = true;
+					audio.load();
+				}
+
+				if ($scope.loaded) {
+					play();
+				}
+
+				if (!$scope.$$phase) {
+					$scope.$apply();
+				}
+			};
+		}
+	};
+
+});
+
 app.controller('SoundStickersController', function($scope, ngAudio) {
 
 	function loadAudio(name) {
@@ -128,61 +182,4 @@ app.controller('SoundStickersController', function($scope, ngAudio) {
 				audio: loadAudio('Sweetie/finger3') }
 		]
 	});
-});
-
-app.directive('audioSticker', function() {
-
-	return {
-		restrict: 'A',
-		scope: {
-			audioSticker: '='
-		},
-		templateUrl: '/directives/audioSticker/view',
-		link: function ($scope, $el, attrs) {
-
-			var audio = new Audio();
-			audio.preload = 'none';
-
-			$scope.showAudioProgress = false;
-			$scope.played = false;
-			$scope.loaded = false;
-
-			function play() {
-				$scope.played = true;
-				audio.play();
-
-				if (!$scope.$$phase) {
-					$scope.$apply();
-				}
-
-				ga('stickerTracker.send', 'event', 'Sound sticker', 'play', $scope.audioSticker.name);
-			}
-
-			//audio.loadeddata = function() {
-			audio.onloadeddata = function() {
-				alert(1234);
-				$scope.showAudioProgress = false;
-				$scope.loaded = true;
-				play();
-			};
-
-			$el.find('.play')[0].onclick = function() {
-				if (!audio.src) {
-					alert('2_' + $scope.audioSticker.audio);
-					audio.src = $scope.audioSticker.audio;
-					$scope.showAudioProgress = true;
-					audio.load();
-				}
-
-				if ($scope.loaded) {
-					play();
-				}
-
-				if (!$scope.$$phase) {
-					$scope.$apply();
-				}
-			};
-		}
-	};
-
 });
